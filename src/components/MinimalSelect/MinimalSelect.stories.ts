1398ecb7099b7ec822ad01151896b85f8928c77a
web-lizzard <czarnous.adrian@gmail.com>
2023-01-05 13:52:25 +0100

    feat: 🎸 add keyboard accesibility function to select

--- src/components/MinimalSelect/MinimalSelect.stories.ts
+++ src/components/MinimalSelect/MinimalSelect.stories.ts
@@ -1,3 +1,4 @@
+import { ref } from 'vue';
 import MinimalSelect from './MinimalSelect.vue';
 
 import type { Meta, StoryFn } from '@storybook/vue3';
@@ -11,9 +12,26 @@ export default {
   component: MinimalSelect,
 } as Meta<typeof MinimalSelect>;
 
-const Template: StoryFn<typeof MinimalSelect> = () => ({
+const Template: StoryFn<typeof MinimalSelect> = (args) => ({
+  setup() {
+    const currentValue = ref('');
+    const options = [
+      { value: '1', label: 'first' },
+      { value: '2', label: 'second' },
+      { value: '3', label: 'third' },
+      { value: '4', label: 'fourth' },
+      { value: '5', label: 'fifth' },
+    ];
+
+    return {
+      currentValue,
+      options,
+      args,
+    };
+  },
   components: { MinimalSelect },
-  template: '<MinimalSelect  />',
+  template:
+    '<MinimalSelect v-bind="args" v-model="currentValue" :options="options"  />',
 });
 
 export const Primary = Template.bind({});
--- src/components/MinimalSelect/MinimalSelect.vue
+++ src/components/MinimalSelect/MinimalSelect.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import MinimalButton from '@/components/MinimalButton/MinimalButton.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 type SelectOption = {
   value: string;
@@ -12,41 +12,117 @@ type Props = {
   buttonPlaceholder?: string;
   options: SelectOption[];
   modelValue?: string;
+  multiselect?: boolean;
 };
 
 const props = withDefaults(defineProps<Props>(), {
   id: crypto.randomUUID(),
   buttonPlaceholder: 'Placeholder',
   options: () => [],
+  multiselect: false,
 });
 
 const emit = defineEmits<{ (e: 'update:modelValue', value: string): void }>();
 
 const open = ref(false);
+const higlightedIndex = ref(0);
+
+const currentItem = computed(() =>
+  props.options.find(({ value }) => value === props.modelValue)
+);
+
+const placeholder = computed(
+  () => currentItem.value?.label || props.buttonPlaceholder
+);
+
+function handleKeyDown(e: KeyboardEvent) {
+  e.preventDefault();
+  switch (e.code) {
+    case 'Enter':
+    case 'Space':
+      if (!open.value) {
+        toggleListVisibility();
+        return;
+      }
+      handleSelectOption(props.options[higlightedIndex.value].value);
+      break;
+    case 'Escape':
+      open.value = false;
+      break;
+    case 'ArrowDown':
+    case 'ArrowUp':
+      if (!open.value) {
+        toggleListVisibility();
+        break;
+      }
+      handleArrowsDown(e.code);
+      break;
+  }
+}
+
+function handleArrowsDown(code: 'ArrowUp' | 'ArrowDown') {
+  const newValue = code === 'ArrowUp' ? -1 : 1;
+  if (higlightedIndex.value === 0 && newValue - 1) {
+    return;
+  }
+
+  if (newValue === 1 && higlightedIndex.value === props.options.length - 1) {
+    return;
+  }
+
+  higlightedIndex.value += newValue;
+}
 
 function toggleListVisibility() {
   open.value = !open.value;
+}
+
+function handleSelectOption(value: string) {
+  emit('update:modelValue', value);
+  open.value = false;
+}
+
+function higlightOption(index: number) {
+  return index === higlightedIndex.value;
 }
 </script>
 
 <template>
   <div class="minimal-select">
     <MinimalButton
+      @keydown="handleKeyDown"
       class="flex | minimal-select__button"
       data-alternative
       :aria-expanded="open"
+      aria-haspopup="listbox"
       :aria-controls="id"
+      :aria-activedescendant="currentItem?.label || ''"
       role="combobox"
       @click="toggleListVisibility"
-      ><span>{{ buttonPlaceholder }} </span>
-      <span aria-hidden="true" class="minimal-select__arrow">&#8594;</span>
+      ><span>{{ placeholder }} </span>
+      <span aria-hidden="true" :data-opened="open" class="minimal-select__arrow"
+        >&#8594;</span
+      >
     </MinimalButton>
-    <ul :id="id" v-show="open" class="minimal-select__listbox" role="listbox">
-      <li class="minimal-select__item">first</li>
-      <li class="minimal-select__item">second</li>
-      <li class="minimal-select__item">third</li>
-      <li class="minimal-select__item">fourth</li>
-      <li class="minimal-select__item">fifth</li>
+    <ul
+      :id="id"
+      :aria-multiselectable="multiselect"
+      v-show="open"
+      class="minimal-select__listbox"
+      role="listbox"
+    >
+      <li
+        role="option"
+        class="minimal-select__item"
+        v-for="(option, index) in options"
+        :key="option.value"
+        :data-highlighted="higlightOption(index)"
+        :aria-selected="option.value === modelValue"
+        :id="option.value"
+        @click="handleSelectOption(option.value)"
+      >
+        {{ option.label }}
+      </li>
     </ul>
   </div>
 </template>
@@ -54,21 +130,31 @@ function toggleListVisibility() {
 <style lang="scss" scoped>
 .minimal-select {
   position: relative;
-  width: fit-content;
+  max-width: var(--select-max-width, min(24rem, 100%));
 
   &__button {
     --button-padding-block: var(--spacing-4);
-    --gap: var(--spacing-8);
+    justify-content: space-between;
+    width: 100%;
   }
 
   &__listbox {
     position: absolute;
     width: 100%;
-    top: var(--select-list-top-position, 120%);
+    top: var(--select-list-top-position, 130%);
     left: 0;
-    max-height: var(--select-list-max-height, 30rem);
+    max-height: var(--select-list-max-height, 50rem);
+    overflow: auto;
     border: var(--select-list-border-width, 4px) solid
       var(--select-list-border-width, var(--clr-neutral-900));
+  }
+
+  &__arrow {
+    transition: transform 150ms ease;
+
+    &[data-opened='true'] {
+      transform: rotate(90deg);
+    }
   }
 
   &__item {
@@ -77,6 +163,16 @@ function toggleListVisibility() {
     cursor: pointer;
 
     &:hover {
+      background-color: var(--clr-neutral-700);
+      color: var(--clr-neutral-100);
+    }
+
+    &[data-highlighted='true'] {
+      background-color: var(--clr-neutral-700);
+      color: var(--clr-neutral-100);
+    }
+
+    &[aria-selected='true'] {
       background-color: var(--clr-neutral-800);
       color: var(--clr-neutral-100);
     }
